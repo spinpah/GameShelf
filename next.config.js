16b119add7f8c3ed24a743bcf5df10a788f3ad6b
spinpah <aymene16boudjelida@gmail.com>
2025-08-16 15:02:13 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -2,8 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.igdb.com'],
+    domains: [
+      'media.rawg.io', 
+      'images.igdb.com',
+      'steamcdn-a.akamaihd.net',
+      'cdn.cloudflare.steamstatic.com'
+    ],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
